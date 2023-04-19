82b8396c0030297d2c9876701e6ef40ecb835226
Lellisinthewoods <ellinor.romin@hotmail.com>
2023-04-19 13:21:14 +0200

    activeCard also gets deleted from localstorage

--- src/reducers/addCardReducer.js
+++ src/reducers/addCardReducer.js
@@ -1,6 +1,6 @@
 const cardsCollection = {
     newCard: JSON.parse(localStorage.getItem('cards')) || [],
-    activeCard: {}
+    activeCard: JSON.parse(localStorage.getItem('activeCard')) || {}
 }
 
 const addNewCard = (state = cardsCollection, action) => {
@@ -18,12 +18,14 @@ const addNewCard = (state = cardsCollection, action) => {
         case 'CHANGE_TOP':
             console.log(action.payload)
             const topCard = action.payload;
+            localStorage.setItem('activeCard', JSON.stringify(topCard))
             return{
                 ...state,
                 activeCard: topCard //ett nytt kortobjekt, tänkt att skriva över objektet i activeCard
             }
         case 'DELETE_CARD':
             let stateArray = state.newCard;
+            const activeCard = state.activeCard;
             console.log(stateArray);
             for (let i = 0; i < stateArray.length; i++) {
                 const card = stateArray[i];
@@ -32,6 +34,9 @@ const addNewCard = (state = cardsCollection, action) => {
                 : null
             }
             console.log(stateArray);
+            activeCard.cardnumber==action.payload.cardnumber //ta bort activecard om kortet är detsamma
+            ? localStorage.removeItem('activeCard')
+            : null
             localStorage.setItem('cards', JSON.stringify(stateArray))
             //måste vi returnera som vi gör i addcard-funktionen?
             //den uppdaterar men re-renderar inte
